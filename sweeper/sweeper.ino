02004de048e0b77015b536b0c8f14d7ddf9b3409
sunnyqywang <sunnywang950529@hotmail.com>
2015-03-03 10:34:01 -0500

    Sweeper with motors not tested

--- sweeper/sweeper.ino
+++ sweeper/sweeper.ino
@@ -1,20 +1,48 @@
 #include <Servo.h>
 
+const int leftMotorCtrlPin1 = 5;
+const int leftMotorCtrlPin2 = 4;
+const int leftMotorEnablePin = 3;
+
+const int rightMotorCtrlPin1 = 7;
+const int rightMotorCtrlPin2 = 8;
+const int rightMotorEnablePin = 6;
+
 Servo mySweeper;  
 
 void setup() {
+  pinMode(leftMotorCtrlPin1, OUTPUT);
+  pinMode(leftMotorCtrlPin2, OUTPUT);
+  pinMode(leftMotorEnablePin, OUTPUT);
+  
+  pinMode(rightMotorCtrlPin1, OUTPUT);
+  pinMode(rightMotorCtrlPin2, OUTPUT);
+  pinMode(rightMotorEnablePin, OUTPUT);
+  
   mySweeper.attach(8); 
+  
+  digitalWrite(leftMotorCtrlPin1, HIGH);
+  digitalWrite(leftMotorCtrlPin2, LOW);
+  digitalWrite(rightMotorCtrlPin1, HIGH);
+  digitalWrite(rightMotorCtrlPin2, LOW);
+  
+  analogWrite(leftMotorEnablePin, NORMALSPEED);
+  analogWrite(rightMotorEnablePin, NORMALSPEED);
+  
+  delay(2000);
+  
+  mySweeper.write(110);
+  
+  digitalWrite(leftMotorCtrlPin1, HIGH);
+  digitalWrite(leftMotorCtrlPin2, LOW);
+  digitalWrite(rightMotorCtrlPin1, HIGH);
+  digitalWrite(rightMotorCtrlPin2, LOW);
+  
+  delay(2000);
+  
 }
 
 void loop() {
-  
-  // set the servo position  
-  mySweeper.write(45);
-  // wait for the servo to get there 
-  delay(10000);
-  
-  mySweeper.write(135);
-  delay(10000);
   
 }
 
