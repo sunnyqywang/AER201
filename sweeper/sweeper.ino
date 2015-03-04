fd82759f465391aa27ee3146c0a53d2a81fb49ed
sunnyqywang <sunnywang950529@hotmail.com>
2015-03-03 19:54:47 -0500

    With motors

--- sweeper/sweeper.ino
+++ sweeper/sweeper.ino
@@ -8,9 +8,13 @@ const int rightMotorCtrlPin1 = 7;
 const int rightMotorCtrlPin2 = 8;
 const int rightMotorEnablePin = 6;
 
+const int led1 = 10;
+const int led2 = 11;
 Servo mySweeper;  
 
 void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
   pinMode(leftMotorCtrlPin1, OUTPUT);
   pinMode(leftMotorCtrlPin2, OUTPUT);
   pinMode(leftMotorEnablePin, OUTPUT);
@@ -19,31 +23,49 @@ void setup() {
   pinMode(rightMotorCtrlPin2, OUTPUT);
   pinMode(rightMotorEnablePin, OUTPUT);
   
-  mySweeper.attach(8); 
+  mySweeper.attach(12); 
   
-  digitalWrite(leftMotorCtrlPin1, HIGH);
-  digitalWrite(leftMotorCtrlPin2, LOW);
-  digitalWrite(rightMotorCtrlPin1, HIGH);
-  digitalWrite(rightMotorCtrlPin2, LOW);
   
-  analogWrite(leftMotorEnablePin, NORMALSPEED);
-  analogWrite(rightMotorEnablePin, NORMALSPEED);
-  
-  delay(2000);
-  
-  mySweeper.write(110);
-  
-  digitalWrite(leftMotorCtrlPin1, HIGH);
-  digitalWrite(leftMotorCtrlPin2, LOW);
-  digitalWrite(rightMotorCtrlPin1, HIGH);
-  digitalWrite(rightMotorCtrlPin2, LOW);
-  
-  delay(2000);
   
 }
 
 void loop() {
   
+  mySweeper.write(-110);
+  
+  delay(2000);
+  
+  digitalWrite(leftMotorCtrlPin1, HIGH);
+  digitalWrite(leftMotorCtrlPin2, LOW);
+  digitalWrite(rightMotorCtrlPin1, HIGH);
+  digitalWrite(rightMotorCtrlPin2, LOW);
+  
+  analogWrite(leftMotorEnablePin, 200);
+  analogWrite(rightMotorEnablePin, 200);
+  
+  delay(2000);
+  
+  analogWrite(leftMotorEnablePin, 0);
+  analogWrite(rightMotorEnablePin, 0);
+  
+  delay(2000);
+  mySweeper.write(110);
+  delay(2000);
+  
+  digitalWrite(led2, 1);
+  digitalWrite(leftMotorCtrlPin1, LOW);
+  digitalWrite(leftMotorCtrlPin2, HIGH);
+  digitalWrite(rightMotorCtrlPin1, LOW);
+  digitalWrite(rightMotorCtrlPin2, HIGH);
+  analogWrite(leftMotorEnablePin, 200);
+  analogWrite(rightMotorEnablePin, 200);
+  
+  delay(2000);
+  
+  digitalWrite(leftMotorEnablePin, 0);
+  digitalWrite(rightMotorEnablePin, 0);
+  
+  delay(100000000);
 }
 
 
